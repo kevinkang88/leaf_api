3fc9bc980e3b6191128849eb87bccbf697bb13e6
Kevin Kang <kevinkang88@gmail.com>
2015-05-03 22:05:30 -0700

    yo

--- app/controllers/leaf_api_controller.rb
+++ app/controllers/leaf_api_controller.rb
@@ -4,7 +4,7 @@ class LeafApiController < ApplicationController
   def cors_preflight_check
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+    headers['Access-Control-Allow-Headers'] = '*'
     headers['Access-Control-Allow-Origin'] = 'http://localhost:8100'
   end
 
