a564676d7f8e8c23ca743725842e124d3f094bbe
Kevin Kang <kevinkang88@gmail.com>
2015-05-11 10:49:09 -0700

    now trying search func

--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -3,9 +3,9 @@ class SoundcloudController < ApplicationController
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   # http_basic_authenticate_with name: 'admin', password: 'secret'
-  before_filter :restrict_access
-  def search
+  before_filter :restrict_access, :cors_preflight_check
 
+  def search
     client = SoundCloud.new(:client_id => ENV['SC_CLIENT_ID'])
     @tracks = client.get('/tracks', :q => 'buskers',:track_type => 'original')
     render json: @tracks
@@ -19,5 +19,12 @@ class SoundcloudController < ApplicationController
     authenticate_or_request_with_http_token do |token,options|
       ApiKey.exists?(access_token: token)
     end
+  end
+
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:8100'
   end
 end
